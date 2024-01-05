ed1c342cc894228c5d747a85d2053e15c1d272a4
Ethan Parent <ethan.parent1@gmail.com>
2024-01-04 22:33:24 -0600

    Create a beautiful weather chart

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import { Grid, ThemeProvider, Box } from '@mui/material';
+import { Grid, createTheme, ThemeProvider, Box } from '@mui/material';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import CloudIcon from '@mui/icons-material/Cloud';
 import { LineChart } from '@mui/x-charts/LineChart';
+import Paper from '@mui/material/Paper';
+import { ChartsReferenceLine } from '@mui/x-charts/ChartsReferenceLine';
 
 
 export default function Weather({ realtimeData, forecastData }) {
@@ -24,82 +26,114 @@ export default function Weather({ realtimeData, forecastData }) {
   forecastData ? forecastData.timelines.daily.map((day) => tempData.push(day.values.temperatureMax)) : [];
   forecastData ? forecastData.timelines.daily.map((day) => days.push(formatDateWithoutYear(day.time))) : [];
 
+  //Convert temp data to F
+  tempData.map((temp, index) => tempData[index] = (temp * 9 / 5) + 32);
+
+
   const cloudCoverData = [];
   forecastData ? forecastData.timelines.daily.map((day) => cloudCoverData.push(day.values.cloudCoverAvg)) : [];
 
   console.log("tempData is:", tempData);
   console.log("days is:", days);
   console.log("cloudCoverData is:", cloudCoverData);
-  return (
-    <Box>
-      {realtimeData ? (
-        <Grid>
-          <p>The weather in Frisco, TX is: {temperatureF} °F or {temperatureC} °C.</p>
-          <p>Cloud cover is at {cloudCover} %</p>
-          {realtimeData && forecastData && forecastData.timelines.daily.length > 0 ? (
-            <LineChart
-              xAxis={[
-                {
-                  type: 'time',
-                  data: days,
-                  scaleType: 'point',
-                  orientation: 'bottom',
-                }
-              ]}
-              yAxis={[
-                {
-                  type: 'linear',
-                  orientation: 'left',
-                  id: 'temperature',
-                  
-                  name: 'Temperature (°C)',
-                  unit: '°C',
-                },
-                {
-                  type: 'linear',
-                  orientation: 'right',
-                  id: 'cloudCover',
-                  
-                  name: 'Cloud Cover (%)',
-                  unit: '%',
-                },
-              ]}
-              series={[
-                {
-                  type: 'line',
-                  data: tempData,
-                  
-                  yAxisId: 'temperature',
-                  name: 'Temperature',
-                  fill: '#8884d8',
-                  stroke: '#8884d8',
-                },
-                {
-                  type: 'line',
-                  data: cloudCoverData,
-                  
-                  yAxisId: 'cloudCover',
-                  name: 'Cloud Cover',
-                  fill: '#82ca9d',
-                  stroke: '#82ca9d',
-                },
-              ]}
-              width={500}
-              height={300}
-            />
-          ) : (
-            <p>No forecast data available.</p>
-          )}
 
-        </Grid>
-      ) : (
-        <p>Loading weather data...</p>
-      )}
-      {realtimeData ? (
-        <p>{weatherImg}</p>
-      ) : (
-        <p>Loading weather data...</p>
-      )}
-    </Box>
+  // Create a dark theme
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      // You can customize other theme options here
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box >
+        {realtimeData ? (
+          <Grid >
+            <Grid >
+              <p>The weather in Frisco, TX is: {temperatureF} °F or {temperatureC} °C.</p>
+              <p>Cloud cover is at {cloudCover} %</p>
+              {realtimeData ? (
+                <p>{weatherImg}</p>
+              ) : (
+                <p>Loading weather data...</p>
+              )}
+            </Grid>
+
+            <Box
+              sx={{
+                height: 260,
+                borderRadius: 5,
+                p: 2,
+                boxShadow: '0 4px 8px rgba(0, 0, 0, .5)',
+                backgroundImage: 'linear-gradient(to bottom, #3498db, #2980b9)', //blue to blue
+              }}
+            >
+              {realtimeData && forecastData && forecastData.timelines.daily.length > 0 ? (
+                <LineChart
+                  xAxis={[
+                    {
+                      type: 'time',
+                      data: days,
+                      scaleType: 'point',
+                      orientation: 'bottom',
+                      
+                    }
+                  ]}
+                  yAxis={[
+                    {
+                      type: 'linear',
+                      orientation: 'left',
+                      id: 'temperature',
+
+                      name: 'Temperature (°C)',
+                      unit: '°C',
+                      
+                    }
+                  ]}
+                  series={[
+                    {
+                      curve: 'linear',
+                      type: 'line',
+                      data: tempData,
+
+                      yAxisId: 'temperature',
+                      name: 'Temperature',
+                      color: '#fff',
+                      strokeWidth: 3,
+                    }
+                  ]}
+                  padding={{
+                    left: 5,
+                    right: 5,
+                    top: 20,
+                    bottom: 20,
+                  }}
+                  sx={{
+                    path: {
+                      strokeWidth: 4,
+                    },
+                    
+                  }}
+                  
+                >
+                  
+                  
+                </LineChart>
+              ) : (
+                <p>No forecast data available.</p>
+              )}
+
+            </Box>
+
+
+          </Grid>
+        ) : (
+          <p>Loading weather data...</p>
+        )}
+
+      </Box>
+    </ThemeProvider>
   );
+
 };
