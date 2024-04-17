2cf0dec87724d1d994a3636a93ee904eb220fea6
Ethan Parent <ethan.parent1@gmail.com>
2024-04-17 12:47:01 -0500

    Change bash file to be more universal

--- cspHashBuild.sh
+++ cspHashBuild.sh
@@ -7,21 +7,21 @@ if [ -f "$HASHES_FILE" ]; then
     # Append script-src hashes
     script_hashes=$(jq -r 'to_entries | map(select(.key | endswith(".js"))) | map(.value | "\"\(.|tostring)\"") | join(" ")' "$HASHES_FILE")
     if [ -n "$script_hashes" ]; then
-        CSP_HEADER+=" script-src 'self' $script_hashes;"
+        CSP_HEADER="${CSP_HEADER} script-src 'self' $script_hashes;"
     fi
 
     # Append style-src hashes
     style_hashes=$(jq -r 'to_entries | map(select(.key | endswith(".css"))) | map(.value | "\"\(.|tostring)\"") | join(" ")' "$HASHES_FILE")
     if [ -n "$style_hashes" ]; then
-        CSP_HEADER+=" style-src 'self' $style_hashes;"
+        CSP_HEADER="${CSP_HEADER} style-src 'self' $style_hashes;"
     fi
 else
     # Default fallbacks if no hashes are generated
-    CSP_HEADER+=" script-src 'self'; style-src 'self';"
+    CSP_HEADER="${CSP_HEADER} script-src 'self'; style-src 'self';"
 fi
 
 # Additional directives
-CSP_HEADER+=" img-src 'self' https:; font-src 'self' https:; connect-src 'self' https:;"
+CSP_HEADER="${CSP_HEADER} img-src 'self' https:; font-src 'self' https:; connect-src 'self' https:;"
 
 # Write the CSP to the nginx snippet
 echo "add_header Content-Security-Policy \"$CSP_HEADER\" always;" > /usr/local/openresty/nginx/conf/snippets/csp.conf
