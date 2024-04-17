81f83a0d58e7a1f4858a396d1943d735c3bb7ff0
Ethan Parent <ethan.parent1@gmail.com>
2024-04-17 11:42:12 -0500

    Switch from generate to writeBundle

--- vite-csp-plugin.js
+++ vite-csp-plugin.js
@@ -1,42 +1,39 @@
 import { promises as fs } from 'fs';
 import { createHash } from 'crypto';
-import { resolve, join } from 'path';
-import { setTimeout } from 'timers/promises';
+import { resolve } from 'path';
+
 
 export default function viteCSPPlugin() {
-
-    
-  return {
-    name: 'vite:csp-hash',
-    enforce: 'post',
-    async generateBundle(options, bundle) {
-      const hashes = {};
-      const outputDir = options.dir || 'dist';  // Ensure this is correctly set in your Vite config
-      console.log("outputDir is:", outputDir);
-
-      for (const [filename, fileinfo] of Object.entries(bundle)) {
-        if (fileinfo.type === 'asset' && (filename.endsWith('.js') || filename.endsWith('.css'))) {
-          const filePath = resolve(outputDir, filename);
-          console.log("file path is:", filePath)
-          await setTimeout(1000);
-          try {
-            const content = await fs.readFile(filePath, 'utf8');
-            const hash = createHash('sha256').update(content).digest('base64');
-            hashes[filename] = `sha256-${hash}`;
-          } catch (err) {
-            console.error(`Failed to read file ${filePath}: ${err}`);
+    return {
+      name: 'vite:csp-hash',
+      enforce: 'post',
+      writeBundle(outputOptions, bundle) {
+        const hashes = {};
+        const outputDir = outputOptions.dir || 'dist';
+  
+        for (const [filename, fileinfo] of Object.entries(bundle)) {
+          if (fileinfo.type === 'asset' && (filename.endsWith('.js') || filename.endsWith('.css'))) {
+            const filePath = resolve(outputDir, fileinfo.fileName);
+  
+            try {
+              const content = fs.readFileSync(filePath, 'utf8');
+              const hash = createHash('sha256').update(content).digest('base64');
+              hashes[filename] = `sha256-${hash}`;
+            } catch (err) {
+              console.error(`Failed to read file ${filePath}: ${err}`);
+            }
           }
         }
+  
+        // Optionally write hashes to a file
+        if (Object.keys(hashes).length > 0) {
+          const hashesPath = resolve(outputDir, 'hashes.json');
+          fs.writeFileSync(hashesPath, JSON.stringify(hashes, null, 2));
+          console.log(`CSP hashes written to ${hashesPath}`);
+        } else {
+          console.log('No hashes generated.');
+        }
       }
-
-      // Writing hashes to a JSON file
-      if (Object.keys(hashes).length > 0) {
-        const hashesPath = join(outputDir, 'hashes.json');
-        await fs.writeFile(hashesPath, JSON.stringify(hashes, null, 2));
-        console.log(`CSP hashes written to ${hashesPath}`);
-      } else {
-        console.log('No hashes generated.');
-      }
-    }
-  };
-}
+    };
+  }
+  
